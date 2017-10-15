7fc6f97d91af03370b54bbf26c5ea62954063cc5
hangdu <2580392278@qq.com>
2017-10-14 23:20:24 -0400

    First commit

---
+++ app/src/main/java/com/example/hang/socketclient/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.hang.socketclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView response;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextAddress = (EditText) findViewById(R.id.addressEditText);
+        editTextPort = (EditText) findViewById(R.id.portEditText);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
+        response = (TextView) findViewById(R.id.responseTextView);
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client(editTextAddress.getText()
+                        .toString(), Integer.parseInt(editTextPort
+                        .getText().toString()), response);
+                myClient.execute();
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                response.setText("");
+            }
+        });
+    }
+}
